14790639a4e05d43ba9d32d7ae59b4a2af50f9f7
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2023-05-04 15:43:08 +0200

    feat: do not fail if threshold was not reached

--- src/index.ts
+++ src/index.ts
@@ -148,7 +148,7 @@ try {
         }
 
         if (resultReport.thresholdReached) {
-            setFailed(
+            console.warn(
                 `coverage decreased over threshold for ${resultReport.thresholdReached} packages`,
             );
         }
